eb301021d2da699e5b331a158e0b43d6da628f5a
ksIsCute <92867218+ksIsCute@users.noreply.github.com>
2023-06-20 15:15:01 +0000

    added github icon  and added description meta to about.html

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ flask.cli.show_server_banner = lambda *args: None
 import logging
 logging.getLogger("werkzeug").disabled = True
 
-listed_platforms = {'twitch':"twitch.tv/2vww", 'discord':"discord_link", 'kick':"kick_link", 'telegram':"telegramlink", 'twitter':"https://twitter.com/2vwwCa", 'instagram':"instagramlink", 'revolt':"revoltP_link", 
- 'steam':"https://steamcommunity.com/id/ksiscute/"}
+
+# available platforms
+# twitch, discord, kick, elegram, twitter, instagram, revolt, steam, github
+listed_platforms = {'discord':"discord_link",'twitter':"https://twitter.com/2vwwCa", 'github': "https://github.com/ksIsCute/DelicaBot"}
 
 # user = request.cookies.get("u_authorization") or None
 
 app = Flask(__name__, '/static')
+
+@app.before_request
+def before_request():
+  user = request.cookies.get("u_authorization" or None)
 
 @app.route("/")
 def index():
@@ -24,57 +30,57 @@ def index():
 @app.route("/getstarted")
 def get_started():
   user = request.cookies.get("u_authorization") or None
-  return render_template('getstarted.html', user=user)
+  return render_template('getstarted.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/portfolio")
 def portfolio():
   user = request.cookies.get("u_authorization") or None
-  return render_template('portfolio.html', user=user)
+  return render_template('portfolio.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/staff")
 def staff():
   user = request.cookies.get("u_authorization") or None
-  return render_template('staff.html', user=user)
+  return render_template('staff.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/showcase")
 def showcase():
   user = request.cookies.get("u_authorization") or None
-  return render_template('showcase.html', user=user)
+  return render_template('showcase.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/support")
 def support():
   user = request.cookies.get("u_authorization") or None
-  return render_template('support.html', user=user)
+  return render_template('support.html', user=user, listed_platforms=listed_platforms)
 
-@app.route("/profile")
-def profile():
+@app.route("/profile/<username>")
+def profile(username):
   user = request.cookies.get("u_authorization") or None
-  return render_template('profile.html', user=user)
+  return render_template('profile.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/settings")
 def settings():
   user = request.cookies.get("u_authorization") or None
-  return render_template('settings.html', user=user)
+  return render_template('settings.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/pricing")
 def pricing():
   user = request.cookies.get("u_authorization") or None
-  return render_template('pricing.html', user=user)
+  return render_template('pricing.html', user=user, listed_platforms=listed_platforms)
 
 @app.route("/signin")
 def signin():
   user = request.cookies.get("u_authorization") or None
-  return render_template('userform.html', user=user, signing="in")
+  return render_template('userform.html', user=user, signing="in", listed_platforms=listed_platforms)
 
 @app.route("/signup")
 def signup():
   user = request.cookies.get("u_authorization") or None
-  return render_template('userform.html', user=user, signing="up")
+  return render_template('userform.html', user=user, signing="up", listed_platforms=listed_platforms)
 
 @app.route("/about")
 def about():
   user = request.cookies.get("u_authorization") or None
-  return render_template('about.html', user=user)
+  return render_template('about.html', user=user, listed_platforms=listed_platforms)
   
 @app.route("/signout")
 def signout():
